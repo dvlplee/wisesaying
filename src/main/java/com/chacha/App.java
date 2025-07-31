595f194b507b3c8d25c39bf0835f895674a1fb97
dvlplee <dvlplee16@gmail.com>
2025-07-31 12:32:31 +0900

    rq in app

--- src/main/java/com/chacha/App.java
+++ src/main/java/com/chacha/App.java
@@ -1,7 +1,6 @@
 package com.chacha;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
@@ -19,17 +18,23 @@ public class App {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if (cmd.equals("종료")) {
-                System.out.println("프로그램을 종료합니다");
-                break;
-            } else if (cmd.equals("등록")) {
-                actionWrite();
-            } else if (cmd.equals("목록")) {
-                actionList();
-            } else if (cmd.startsWith("삭제")) { // 삭제?id=1
-                actionDelete(cmd);
-            } else if (cmd.startsWith("수정")) {
-                actionModify(cmd);
+            Rq rq = new Rq(cmd);
+            switch (rq.getActionName()) {
+                case "종료":
+                    System.out.println("프로그램을 종료합니다");
+                    return;
+                case "등록":
+                    actionWrite();
+                    break;
+                case "목록":
+                    actionList();
+                    break;
+                case "삭제":
+                    actionDelete(rq);
+                    break;
+                case "수정":
+                    actionModify(rq);
+                    break;
             }
         }
     }
@@ -58,19 +63,23 @@ public class App {
 //            System.out.println(w.getId() + " / " + w.getAuthor() + " / " + w.getContent());
 //        }
         IntStream.range(0, list.size())
-                .map(i ->list.size() -1 -i)
+                .map(i -> list.size() - 1 - i)
                 .mapToObj(list::get)
                 .forEach(ws -> System.out.println(
                         ws.getId() + " / " + ws.getAuthor() + " / " + ws.getContent()
                 ));
     }
 
-    void actionDelete(String cmd) {
-        int id = CmdSplitId(cmd);
-        if (id < 0) return;
+    void actionDelete(Rq rq) {
+        int id = rq.getParamAsInt("id", -1);
+        if (id == -1) {
+            System.out.println("숫자를 입력해주세요.");
+            return;
+        }
 
         WiseSaying wiseSaying = findById(id);
         if (wiseSaying == null) return;
+
         delete(wiseSaying);
 
         System.out.println(id + "번 명언이 삭제되었습니다.");
@@ -81,9 +90,9 @@ public class App {
         // list.removeIf(ws -> ws.getId() == wiseSaying.getId());
     }
 
-    void actionModify(String cmd) {
-        int id = CmdSplitId(cmd);
-        if (id < 0) return;
+    void actionModify(Rq rq) {
+        int id = rq.getParamAsInt("id", -1);
+        if (id == -1) return;
 
         WiseSaying wiseSaying = findById(id);
         if (wiseSaying == null) return;
@@ -117,36 +126,18 @@ public class App {
 //            return null;
 //        }
 //        return wiseSaying;
+
         // 1. 스트림 생성
         // 2. 필터링
         // 3. 첫 번째 요소 찾아서 Optional 객체로 감싸서 반환
         // (Optional 객체는 값이 없을 수도 있을 수도 있음)
         // 4. Optional 객체가 있으면 그대로 반환, 없으면 람다식 실행
         return list.stream()
-                .filter(ws->ws.getId()==id)
+                .filter(ws -> ws.getId() == id)
                 .findFirst()
                 .orElseGet(() -> {
-            System.out.println("해당 아이디는 존재하지 않습니다");
-            return null;
-        });
-    }
-
-    int CmdSplitId(String cmd) {
-//        String[] cmdBits = cmd.split("="); // 삭제?id=1
-//
-//        if (cmdBits.length < 2 || cmdBits[1].isEmpty()) {
-//            System.out.println("id를 입력해주세요.");
-//            return -1;
-//        }
-//        return Integer.parseInt(cmdBits[1]);
-        return Arrays.stream(cmd.split("="))
-                .skip(1)
-                .findFirst()
-                .filter(s-> !s.isEmpty())
-                .map(Integer::parseInt)
-                .orElseGet(() -> {
-                    System.out.println("id를 입력해주세요.");
-                    return -1;
+                    System.out.println("해당 아이디는 존재하지 않습니다");
+                    return null;
                 });
     }
 }
--- src/main/java/com/chacha/Main.java
+++ src/main/java/com/chacha/Main.java
@@ -2,24 +2,7 @@ package com.chacha;
 
 public class Main {
     public static void main(String[] args) {
-//        App app = new App();
-//        app.run();
-
-        testRq1();
-    }
-    private static void testRq1() {
-        Rq rq = new Rq("목록?searchKeywordType=content&searchKeyword=자바&page=9");
-        int page = rq.getParamAsInt("page", -1);
-        int sort = rq.getParamAsInt("sort", -1);
-        int id = rq.getParamAsInt("id", -1);
-
-        System.out.println(rq.getActionName());
-        System.out.println(rq.getParam("searchKeywordType", ""));
-        System.out.println(rq.getParam("searchKeyword", ""));
-        System.out.println(page);
-        System.out.println(sort);
-        System.out.println(id);
-
-//        String page = rq.getParam("page", "");
+        App app = new App();
+        app.run();
     }
 }
